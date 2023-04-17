5820be7848748c03c852ce8d34c69e82f933faf2
Tạ Quốc Bảo <77446975+tabao0810@users.noreply.github.com>
2023-04-17 22:06:24 +0700

    updated

--- server/routes/banners.js
+++ server/routes/banners.js
@@ -1,14 +1,18 @@
 const express = require("express");
 
-const { createBanner,getAllBanner,deleteBanner } = require('../controllers/banners');
+const {
+  createBanner,
+  getAllBanner,
+  deleteBanner,
+} = require("../controllers/banners");
 
 const routerBanner = express.Router();
 routerBanner.post("/banners/create", createBanner);
-routerBanner.get('/banners', getAllBanner);
+routerBanner.get("/banners", getAllBanner);
 // routerBanner.get('/blogs/:blogId',getSingleBlog);
 
 // routerBanner.put('/blogs/:blogId/update', updateBlog);
 
-routerBanner.delete('/banners/:bannerId', deleteBanner);
+routerBanner.delete("/banners/:bannerId", deleteBanner);
 
 module.exports = routerBanner;
